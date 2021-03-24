b8795206ac7a010800bf3bda46b53d4095010409
moein <Moeinabdi77@gmail.com>
2021-03-24 11:56:40 +0430

    add DI modules

---
+++ app/src/main/java/ir/moeindeveloper/instaweather/core/di/RepositoryModule.kt
@@ -0,0 +1,11 @@
+package ir.moeindeveloper.instaweather.core.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class RepositoryModule {
+    //Put repos here
+}
