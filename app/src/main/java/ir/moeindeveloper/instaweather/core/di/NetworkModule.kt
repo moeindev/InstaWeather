9eadabec2cd949bbfca785f2984f4faa34566dbe
moein <Moeinabdi77@gmail.com>
2021-04-08 12:45:55 +0430

    Fill DI modules

--- app/src/main/java/ir/moeindeveloper/instaweather/core/di/NetworkModule.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/core/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import ir.moeindeveloper.instaweather.feature.common.service.WeatherService
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -15,6 +16,12 @@ import javax.inject.Singleton
 object NetworkModule {
 
     private const val BASE_URL: String = "https://api.openweathermap.org/data/2.5/"
+
+    @Provides
+    @Singleton
+    fun provideWeatherService(retrofit: Retrofit): WeatherService =
+        retrofit.create(WeatherService::class.java)
+
 
     @Provides
     @Singleton
--- app/src/main/java/ir/moeindeveloper/instaweather/core/di/RepositoryModule.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/core/di/RepositoryModule.kt
@@ -1,11 +1,22 @@
 package ir.moeindeveloper.instaweather.core.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+import io.objectbox.BoxStore
+import ir.moeindeveloper.instaweather.feature.common.repository.WeatherRepository
+import ir.moeindeveloper.instaweather.feature.common.repository.WeatherRepositoryImpl
+import ir.moeindeveloper.instaweather.feature.common.service.WeatherService
 
 @Module
 @InstallIn(ViewModelComponent::class)
 class RepositoryModule {
-    //Put repos here
+
+    @Provides
+    @ViewModelScoped
+    fun provideWeatherRepository(service: WeatherService, store: BoxStore): WeatherRepository =
+        WeatherRepositoryImpl(service,store)
+
 }
