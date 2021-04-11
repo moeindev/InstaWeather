b3368e7851e7e52cc8d33b55b7ecf6c791877541
moein <Moeinabdi77@gmail.com>
2021-04-11 13:04:07 +0430

    change app theme

--- app/src/main/java/ir/moeindeveloper/instaweather/ui/theme/Color.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/ui/theme/Color.kt
@@ -2,7 +2,12 @@ package ir.moeindeveloper.instaweather.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val primaryColor = Color(0xFFfafafa)
+val primaryLight = Color(0xFFffffff)
+val primaryDark = Color(0xFFc7c7c7)
+val onPrimary = Color(0xFF000000)
+
+val secondaryColor = Color(0xFF212121)
+val secondaryLight = Color(0xFF484848)
+val secondaryDark = Color(0xFF000000)
+val onSecondary = Color(0xFFffffff)
--- app/src/main/java/ir/moeindeveloper/instaweather/ui/theme/Type.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/ui/theme/Type.kt
@@ -9,20 +9,18 @@ import androidx.compose.ui.unit.sp
 // Set of Material typography styles to start with
 val Typography = Typography(
         body1 = TextStyle(
-                fontFamily = FontFamily.Default,
+                fontFamily = FontFamily.Monospace,
                 fontWeight = FontWeight.Normal,
                 fontSize = 16.sp
+        ),
+        button = TextStyle(
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.W500,
+                fontSize = 14.sp
+        ),
+        caption = TextStyle(
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Normal,
+                fontSize = 12.sp
         )
-        /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
