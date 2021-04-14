868460451365876857f4fd16c25e9e4260ce0476
moein <Moeinabdi77@gmail.com>
2021-04-14 20:23:12 +0430

    provide viewModel inside navHost

--- app/src/main/java/ir/moeindeveloper/instaweather/ui/navigation/NavigationCompose.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/ui/navigation/NavigationCompose.kt
@@ -1,9 +1,11 @@
 package ir.moeindeveloper.instaweather.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltNavGraphViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import ir.moeindeveloper.instaweather.feature.common.viewModel.WeatherViewModel
 import ir.moeindeveloper.instaweather.feature.home.ui.HomeScreen
 import ir.moeindeveloper.instaweather.feature.home.ui.homeDestName
 
@@ -12,7 +14,8 @@ fun SetupNavigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = homeDestName) {
         composable(homeDestName) {
-            HomeScreen(navController = navController)
+            val weatherViewModel: WeatherViewModel = hiltNavGraphViewModel()
+            HomeScreen(mainViewModel = weatherViewModel,navController = navController)
         }
 
         //TODO add other screens here
