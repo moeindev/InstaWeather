a5981f03e734543d2434666be527384f9186bf9f
moein <Moeinabdi77@gmail.com>
2021-04-14 20:19:02 +0430

    deleted uiState provider

--- app/src/main/java/ir/moeindeveloper/instaweather/ui/provider/FakeUiStateProvider.kt
+++
@@ -1,7 +0,0 @@
-package ir.moeindeveloper.instaweather.ui.provider
-
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import ir.moeindeveloper.instaweather.core.state.UiState
-
-class FakeUiStateProvider(override val values: Sequence<UiState>) :
-    PreviewParameterProvider<UiState>
