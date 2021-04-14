158d011c7e8bdc4ab250d9764ac884b19ca52728
moein <Moeinabdi77@gmail.com>
2021-04-14 20:26:09 +0430

    Design Home Screen. closes #15

--- app/src/main/java/ir/moeindeveloper/instaweather/feature/home/ui/HomeUi.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/feature/home/ui/HomeUi.kt
@@ -1,12 +1,73 @@
 package ir.moeindeveloper.instaweather.feature.home.ui
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import ir.moeindeveloper.instaweather.core.log.appLog
+import ir.moeindeveloper.instaweather.core.state.UiState
+import ir.moeindeveloper.instaweather.core.state.UiStatus
+import ir.moeindeveloper.instaweather.feature.common.entity.WeatherInfoBox
+import ir.moeindeveloper.instaweather.feature.common.util.toStringTemp
+import ir.moeindeveloper.instaweather.feature.common.util.weatherType
+import ir.moeindeveloper.instaweather.feature.common.viewModel.WeatherViewModel
+import ir.moeindeveloper.instaweather.ui.common.Failed
+import ir.moeindeveloper.instaweather.ui.common.Loader
 
 const val homeDestName: String = "home_screen"
 
 @Composable
-fun HomeScreen(navController: NavController) {
-    Text(text = "WELCOME TO HOME SCREEN")
+fun HomeScreen(mainViewModel: WeatherViewModel,navController: NavController) {
+
+    val weatherData = mainViewModel.data.collectAsState()
+
+    weatherData.value?.let { data ->
+        data.appLog { "data = $data" }
+        when(data.status) {
+            UiStatus.LOADING -> Loader()
+            UiStatus.Failure -> Failed(reason = data.errorMessage ?: "") {
+                mainViewModel.getWeatherData()
+            }
+            UiStatus.SUCCESS -> {
+                data.data?.let { weatherInfo ->
+                    data.appLog { "data from repo -> $data" }
+                    HomeSuccess(data = weatherInfo)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeSuccess(data: WeatherInfoBox) {
+    Column(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth(),
+        verticalArrangement = Arrangement.SpaceEvenly) {
+        //TODO add lastUpdate
+        HomeHeader(lastUpdate = "Someday on March 27", city = data.timezone) {
+            //TODO open settings
+        }
+
+        HomeBackground(
+            isNight = false,
+            type = data.current.target.weather[0].icon.weatherType()
+        )
+
+        HomeCurrent(
+            description = data.current.target.weather[0].description,
+            temperature = data.current.target.temp.toStringTemp(),
+            //TODO add units
+            unit = "C",
+            feelsLike = data.current.target.feelsLike.toStringTemp(),
+            dewPoint = data.current.target.feelsLike.toStringTemp()
+        )
+
+        HourlyContainer(hours = data.hourly)
+    }
 }
