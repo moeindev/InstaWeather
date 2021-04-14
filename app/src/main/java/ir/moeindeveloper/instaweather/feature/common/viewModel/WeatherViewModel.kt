bd612b31212474bc5a84b5f380cb1843a3d89849
moein <Moeinabdi77@gmail.com>
2021-04-14 20:22:05 +0430

    implement new changes from the repository

--- app/src/main/java/ir/moeindeveloper/instaweather/feature/common/viewModel/WeatherViewModel.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/feature/common/viewModel/WeatherViewModel.kt
@@ -1,50 +1,48 @@
 package ir.moeindeveloper.instaweather.feature.common.viewModel
 
-import androidx.compose.ui.unit.Constraints
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ir.moeindeveloper.instaweather.core.extension.transformToFlow
+import ir.moeindeveloper.instaweather.core.log.appLog
 import ir.moeindeveloper.instaweather.core.platform.viewModel.BaseViewModel
+import ir.moeindeveloper.instaweather.core.state.BackgroundState
+import ir.moeindeveloper.instaweather.core.state.BackgroundStatus
 import ir.moeindeveloper.instaweather.core.state.UiState
 import ir.moeindeveloper.instaweather.feature.common.Constant
 import ir.moeindeveloper.instaweather.feature.common.entity.WeatherInfoBox
 import ir.moeindeveloper.instaweather.feature.common.repository.WeatherRepository
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class WeatherViewModel @Inject constructor(private val repository: WeatherRepository): BaseViewModel() {
 
-    private val _uiState = MutableStateFlow<UiState>(UiState.Loading)
-    val uiState: StateFlow<UiState> get() = _uiState
-
     private val weatherLoadingChannel: Channel<HashMap<String,Any>> = Channel()
 
-    val data: StateFlow<WeatherInfoBox?> = scope {
+    val data: StateFlow<UiState<WeatherInfoBox>?> = scope {
         weatherLoadingChannel.transformToFlow { channel ->
             emitAll(
                 repository.retrieveWeatherData(
                     latitude = channel[lat] as Double,
                     longitude = channel[lon] as Double,
                     exclude = channel[exclude] as String,
-                    units = channel[units] as String,
-                    onSuccess = { updateUiState(UiState.Success) },
-                    onError = { updateUiState(UiState.Failure(it)) },
-                    onException = { updateUiState(UiState.Failure(it.localizedMessage)) }
-                )
+                    units = channel[units] as String
+                ).convertToUiState()
             )
         }
+    }
+
+    init {
+        appLog { "initial value of weather data = $data" }
+        getWeatherData()
     }
 
     fun getWeatherData() {
         //TODO get pre-saved data from preferences!
         //for now just use the constants
         viewModelScope.launch {
-            updateUiState(UiState.Loading)
             weatherLoadingChannel.send(
                 hashMapOf(
                     //TODO change these values to dynamic values
@@ -57,12 +55,12 @@ class WeatherViewModel @Inject constructor(private val repository: WeatherReposi
         }
     }
 
-
-    private fun updateUiState(uiState: UiState) {
-        viewModelScope.launch {
-            _uiState.emit(
-                uiState
-            )
+    private fun Flow<BackgroundState<WeatherInfoBox>>.convertToUiState(): Flow<UiState<WeatherInfoBox>> = map { value ->
+        when(value.status) {
+            BackgroundStatus.LOADING ->  UiState.loading()
+            BackgroundStatus.SUCCESS -> UiState.success(value.data)
+            BackgroundStatus.ERROR ->  UiState.failure(value.errorMessage)
+            BackgroundStatus.EXCEPTION -> UiState.failure(value.errorMessage)
         }
     }
 
