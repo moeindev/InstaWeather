3faee582d1d34162c6284e9c6270f0281a31af11
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-05-31 17:36:48 +0430

    feat(walkThrough): write user settings for walkThrough

--- app/src/main/java/ir/moeindeveloper/instaweather/MainActivity.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/MainActivity.kt
@@ -1,14 +1,8 @@
 package ir.moeindeveloper.instaweather
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import ir.moeindeveloper.instaweather.ui.navigation.SetupNavigation
 import ir.moeindeveloper.instaweather.ui.theme.InstaWeatherTheme
