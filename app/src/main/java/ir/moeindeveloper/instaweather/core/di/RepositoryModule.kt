2d3368ba7746d80e9d02ab84dcfc689080de619b
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-06-12 14:36:47 +0430

    feat(walkThrough): write ip location repository

--- app/src/main/java/ir/moeindeveloper/instaweather/core/di/RepositoryModule.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/core/di/RepositoryModule.kt
@@ -6,8 +6,11 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
 import io.objectbox.BoxStore
+import ir.moeindeveloper.instaweather.feature.common.repository.IpLocationRepository
+import ir.moeindeveloper.instaweather.feature.common.repository.IpLocationRepositoryImpl
 import ir.moeindeveloper.instaweather.feature.common.repository.WeatherRepository
 import ir.moeindeveloper.instaweather.feature.common.repository.WeatherRepositoryImpl
+import ir.moeindeveloper.instaweather.feature.common.service.IpLocationService
 import ir.moeindeveloper.instaweather.feature.common.service.WeatherService
 
 @Module
@@ -19,4 +22,9 @@ class RepositoryModule {
     fun provideWeatherRepository(service: WeatherService, store: BoxStore): WeatherRepository =
         WeatherRepositoryImpl(service,store)
 
+
+    @Provides
+    @ViewModelScoped
+    fun provideIpLocationRepository(service: IpLocationService): IpLocationRepository =
+        IpLocationRepositoryImpl(service = service)
 }
