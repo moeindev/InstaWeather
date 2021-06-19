50c3a9a20677a69ccf0119581148092a5b6a19ac
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-06-19 23:31:02 +0430

    feat(walkThrough): write IP location based state's ui

--- app/src/main/java/ir/moeindeveloper/instaweather/feature/walkthrough/ui/Location.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/feature/walkthrough/ui/Location.kt
@@ -5,16 +5,30 @@ import android.content.pm.PackageManager
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
+import com.google.accompanist.coil.rememberCoilPainter
+import com.skydoves.whatif.whatIfNotNull
 import ir.moeindeveloper.instaweather.R
+import ir.moeindeveloper.instaweather.core.state.UiState
+import ir.moeindeveloper.instaweather.core.state.UiStatus
 import ir.moeindeveloper.instaweather.feature.common.extensions.GoogleAvailability
 import ir.moeindeveloper.instaweather.feature.walkthrough.viewModel.WalkThroughViewModel
 const val FindLocationNavDest: String = "find_location"
@@ -37,7 +51,9 @@ fun FindLocation(viewModel: WalkThroughViewModel, navController: NavController, 
         mutableStateOf(true)
     }
 
-    Column(modifier = Modifier.fillMaxSize(),
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(state = ScrollState(0)),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween) {
 
@@ -82,7 +98,57 @@ fun FindLocation(viewModel: WalkThroughViewModel, navController: NavController, 
 
 @Composable
 fun UseIpLocation(viewModel: WalkThroughViewModel, onUseGMS: () -> Unit) {
+    val locationState = viewModel.ipLocation.collectAsState()
 
+    locationState.value.whatIfNotNull { state ->
+        when(state.status) {
+            UiStatus.LOADING -> {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    FindingYourLocationText()
+                    LocationButton(id = R.string.use_gps) {
+                        onUseGMS()
+                    }
+                }
+            }
+
+            UiStatus.Failure -> {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(text = stringResource(id = R.string.find_location),
+                        style = MaterialTheme.typography.body1,
+                        color = MaterialTheme.colors.onPrimary)
+
+                    LocationButton(id = R.string.use_gps) {
+                        onUseGMS()
+                    }
+                }
+            }
+
+            UiStatus.SUCCESS -> {
+                state.data.whatIfNotNull { ipLocation ->
+                    val locStr = "${ipLocation.query} \n ${ipLocation.city}, ${ipLocation.country}"
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        Text(text = stringResource(id = R.string.your_location_is),
+                            style = MaterialTheme.typography.body1,
+                            color = MaterialTheme.colors.onPrimary)
+
+                        Row(modifier = Modifier.fillMaxWidth()) {
+
+                            Image(painter = rememberCoilPainter(request = "https://www.countryflags.io/${ipLocation.countryCode.lowercase()}/shiny/64.png")
+                                , contentDescription = ipLocation.country)
+                            
+                            Text(text = locStr,
+                                style = MaterialTheme.typography.body1,
+                                color = MaterialTheme.colors.onPrimary)
+
+                            LocationButton(id = R.string.use_gps) {
+                                onUseGMS()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -99,6 +165,23 @@ fun GMSNotInstalled(onUseIpLocation: () -> Unit) {
 @Composable
 fun GMSNeedUpdate(onUseIpLocation: () -> Unit) {
 
+}
+
+@Composable
+fun LocationButton(@StringRes id: Int, onClick: () -> Unit) {
+    Button(onClick = { onClick() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)) {
+        Text(text = stringResource(id = id))
+    }
+}
+
+@Composable
+fun FindingYourLocationText() {
+    Text(text = stringResource(id = R.string.find_location),
+        style = MaterialTheme.typography.body1,
+        color = MaterialTheme.colors.onPrimary)
 }
 
 //@Preview(showSystemUi = true)
