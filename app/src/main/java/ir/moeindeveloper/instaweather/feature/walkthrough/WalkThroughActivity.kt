9dd821c50c5494fd47fbd5638f0abdd2365bea36
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-06-26 18:42:39 +0430

    feat(walkThrough): attach walkThrough navigation to the activity

--- app/src/main/java/ir/moeindeveloper/instaweather/feature/walkthrough/WalkThroughActivity.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/feature/walkthrough/WalkThroughActivity.kt
@@ -2,9 +2,25 @@ package ir.moeindeveloper.instaweather.feature.walkthrough
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.ExperimentalMaterialApi
+import dagger.hilt.android.AndroidEntryPoint
+import ir.moeindeveloper.instaweather.feature.walkthrough.ui.SetupWalkThroughNavigation
+import ir.moeindeveloper.instaweather.ui.theme.InstaWeatherTheme
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@AndroidEntryPoint
 class WalkThroughActivity : AppCompatActivity() {
+    @ExperimentalCoroutinesApi
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            InstaWeatherTheme {
+                SetupWalkThroughNavigation {
+                    finish()
+                }
+            }
+        }
     }
 }
