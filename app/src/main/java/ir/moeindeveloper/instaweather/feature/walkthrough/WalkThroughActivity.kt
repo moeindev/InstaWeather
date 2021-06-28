30ac5b00e1b6dd9a742b47174447d23587e5eb59
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-06-28 21:05:10 +0430

    feat(walkThrough): remove redundant imports

--- app/src/main/java/ir/moeindeveloper/instaweather/feature/walkthrough/WalkThroughActivity.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/feature/walkthrough/WalkThroughActivity.kt
@@ -4,21 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.material.ExperimentalMaterialApi
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.LocationSettingsResponse
-import com.google.android.gms.location.LocationSettingsStatusCodes
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
 import dagger.hilt.android.AndroidEntryPoint
-import ir.moeindeveloper.instaweather.R
-import ir.moeindeveloper.instaweather.core.toast.toaster
-import ir.moeindeveloper.instaweather.feature.common.location.LocationProvider.Companion.REQUEST_CHECK_SETTINGS
 import ir.moeindeveloper.instaweather.feature.walkthrough.ui.SetupWalkThroughNavigation
 import ir.moeindeveloper.instaweather.ui.theme.InstaWeatherTheme
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import javax.inject.Inject
-
 @AndroidEntryPoint
 class WalkThroughActivity : AppCompatActivity() {
 
