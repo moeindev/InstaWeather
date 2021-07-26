0f8a2e1f5770be80cdda4c8000be030c236bad6a
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-07-26 12:27:17 +0430

    feat(settings): add settings page to navigation component

--- app/src/main/java/ir/moeindeveloper/instaweather/ui/navigation/NavigationCompose.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/ui/navigation/NavigationCompose.kt
@@ -1,5 +1,6 @@
 package ir.moeindeveloper.instaweather.ui.navigation
 
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
@@ -8,7 +9,11 @@ import androidx.navigation.compose.rememberNavController
 import ir.moeindeveloper.instaweather.feature.common.viewModel.WeatherViewModel
 import ir.moeindeveloper.instaweather.feature.home.ui.HomeScreen
 import ir.moeindeveloper.instaweather.feature.home.ui.homeDestName
+import ir.moeindeveloper.instaweather.feature.walkthrough.settings.Settings
+import ir.moeindeveloper.instaweather.feature.walkthrough.settings.settingsDestName
+import ir.moeindeveloper.instaweather.feature.walkthrough.viewModel.WalkThroughViewModel
 
+@ExperimentalMaterialApi
 @Composable
 fun SetupNavigation() {
     val navController = rememberNavController()
@@ -18,6 +23,9 @@ fun SetupNavigation() {
             HomeScreen(mainViewModel = weatherViewModel,navController = navController)
         }
 
-        //TODO add other screens here
+        composable(settingsDestName) {
+            val walkThroughViewModel: WalkThroughViewModel = hiltViewModel()
+            Settings(viewModel = walkThroughViewModel)
+        }
     }
 }
