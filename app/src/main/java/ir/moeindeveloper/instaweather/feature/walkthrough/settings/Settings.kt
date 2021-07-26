1b61efdc27dd20cda13ab509caf7b465f89a6e47
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-07-26 12:28:43 +0430

    feat(settings): remove redundant imports

--- app/src/main/java/ir/moeindeveloper/instaweather/feature/walkthrough/settings/Settings.kt
+++ app/src/main/java/ir/moeindeveloper/instaweather/feature/walkthrough/settings/Settings.kt
@@ -1,21 +1,17 @@
 package ir.moeindeveloper.instaweather.feature.walkthrough.settings
 
-import androidx.annotation.StringRes
+
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.PopupProperties
 import ir.moeindeveloper.instaweather.MainActivity
 import ir.moeindeveloper.instaweather.R
 import ir.moeindeveloper.instaweather.feature.common.preferences.Settings
